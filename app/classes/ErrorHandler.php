feb003819836b6a8c66775bdab51b5b9ff3b6587
Anderson Souza <dev.andersondesouzafeitosa@gmail.com>
2025-05-23 13:44:25 -0300

    Tratando exceções

---
+++ app/classes/ErrorHandler.php
@@ -0,0 +1,51 @@
+<?php
+namespace app\classes;
+
+use Error;
+use ErrorException;
+use Throwable;
+
+class ErrorHandler{
+    
+    public static function register(){
+        set_exception_handler([self::class, 'handleException']);
+        set_error_handler([self::class, 'handleError']);
+        register_shutdown_function([self::class, 'handleShutdown']);
+    }
+
+    public static function handleException(Throwable $e){
+        error_log($e);
+        http_response_code(500);
+        echo 'Erro interno.';
+    }
+
+    public static function handleError($severity, $message, $file, $line){
+        throw new ErrorException($message, 0, $severity, $file, $line);
+    }
+
+    public static function log(Throwable $e):void{
+        $message = sprintf(
+            "[%s] %s in %s:%d\nTrace:\n%s\n\n",
+            date('Y-m-d H:i:s'),
+            $e->getMessage(),
+            $e->getFile(),
+            $e->getLine(),
+            $e->getTraceAsString()
+        );
+        file_put_contents(dirname(__DIR__ . "/../../logs/error.log"), $message, FILE_APPEND);
+    }
+
+    public static function handleShutdown()
+    {
+        $error = error_get_last();
+        if ($error && in_array($error['type'], [E_ERROR, E_PARSE, E_CORE_ERROR, E_COMPILE_ERROR])) {
+            $e = new \ErrorException($error['message'], 0, $error['type'], $error['file'], $error['line']);
+            self::log($e);
+
+            if (http_response_code() === 200) {
+                http_response_code(500);
+            }
+            echo "Erro fatal.";
+        }
+    }
+}
--- index.php
+++ index.php
@@ -1,14 +1,29 @@
 <?php
 
+
 require_once __DIR__ . "/vendor/autoload.php";
 require_once __DIR__ . "/config/config.php";
 
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ );
 $dotenv->load();
-
 use app\core\Core;
+use app\classes\ErrorHandler;
 
 $core = new Core();
+try{
 
-$core->run();
+    $core->run();
+
+}catch(\Exception $e){
+
+    ErrorHandler::log($e); 
+    http_response_code(500);
+    echo "Erro interno do servidor (BD).";
+
+}catch(\Throwable $e){
+    ErrorHandler::log($e);
+    http_response_code(500);
+    echo "Erro interno do servidor.";
+}
+
 
