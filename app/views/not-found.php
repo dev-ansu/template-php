e3fd08bd26bd8800d8fc0540b24a3e192ec8016f
Anderson Souza <dev.andersondesouzafeitosa@gmail.com>
2025-05-23 20:17:53 -0300

    View de not found

---
+++ app/views/not-found.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+    <title><?= $title ?? 'Document' ?></title>
+</head>
+<body>
+
+    <main class="h-screen w-full bg-slate-900 flex justify-center items-center p-4">
+        <span class="text-white font-thin text-xl">404 | Página não encontrada</span>        
+    </main>
+
+
+</body>
+</html>
